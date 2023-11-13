614c40a58d8797eb68a960202215e464791b2dd6
artingo <alfred.walther@artingo.net>
2023-11-13 23:51:14 +0100

    Step 6: Create a layout template

---
+++ public/index.php
@@ -0,0 +1,1 @@
+<?php require("../controllers/posts.php"); ?>
---
+++ views/partials/top-navbar.php
@@ -0,0 +1,56 @@
+<nav class="main-header navbar navbar-expand navbar-light">
+    <ul class="navbar-nav">
+        <li class="nav-item">
+            <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+        </li>
+        <li class="nav-item d-none d-sm-inline-block">
+            <a href="#" class="nav-link">Posts</a>
+        </li>
+        <li class="nav-item d-none d-sm-inline-block  dropdown">
+            <a href="#" class="nav-link dropdown-toggle" id="navbarDropdown2" role="button"
+               data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Categories
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown2">
+                <?php foreach ($GLOBALS['categories'] as $category) : ?>
+                    <a class="dropdown-item" href="#"><?= $category->name ?></a>
+                <?php endforeach; ?>
+
+            </div>
+        </li>
+    </ul>
+
+    <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+            <a class="nav-link" data-widget="navbar-search" href="#" role="button">
+                <i class="nav-icon fas fa-search"></i>
+            </a>
+            <div class="navbar-search-block">
+                <form class="form-inline">
+                    <div class="input-group input-group-sm">
+                        <input class="form-control form-control-navbar" type="search" placeholder="Search"
+                               aria-label="Search">
+                        <div class="input-group-append">
+                            <button class="btn btn-navbar" type="submit">
+                                <i class="fas fa-search"></i>
+                            </button>
+                            <button class="btn btn-navbar" type="button" data-widget="navbar-search">
+                                <i class="fas fa-times"></i>
+                            </button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" data-toggle="dropdown" href="#">
+                <i class="fas fa-users"></i>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" data-widget="fullscreen" href="#" role="button">
+                <i class="fas fa-expand-arrows-alt"></i>
+            </a>
+        </li>
+    </ul>
+</nav>
