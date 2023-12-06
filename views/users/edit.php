165f79d620ca6d2b09c561ce1db038ff7597e0d3
Seweryn Czabanowski <seweryn_cz@outlook.com>
2023-12-06 21:01:14 +0100

    Remove password values in inputs

--- views/users/edit.php
+++ views/users/edit.php
@@ -10,72 +10,60 @@
 
             <div class="card card-indigo card-outline">
                 <form method="post" action="/users/save" id="userForm">
-                    <input type="hidden" name="isExistingUser" value="<?= $isExistingUser ?>"><br/>
+                    <input type="hidden" name="isExistingUser" value="<?= $isExistingUser ?>"><br />
                     <input type="hidden" name="id" value="<?= $user->id ?>">
 
                     <div class="card-body">
                         <div class="form-group row">
                             <div class="col-sm-2">
-                                <img class="profile-user-img img-fluid img-circle" width="128"
-                                     src="<?= $user->avatarUrl ?>" alt="User profile picture"><br/>
+                                <img class="profile-user-img img-fluid img-circle" width="128" src="<?= $user->avatarUrl ?>" alt="User profile picture"><br />
                             </div>
                             <div class="col-sm-10">
                                 <label for="avatarUrl">Avatar URL</label>
-                                <input type="text" class="form-control" name="avatarUrl" id="avatarUrl"
-                                       value="<?= $user->avatarUrl ?>" placeholder="Paste your avatar URL here...">
+                                <input type="text" class="form-control" name="avatarUrl" id="avatarUrl" value="<?= $user->avatarUrl ?>" placeholder="Paste your avatar URL here...">
                             </div>
                         </div>
 
                         <div class="form-group">
                             <div class="offset-md-2 col-sm-10">
                                 <label for="name">Username</label>
-                                <input type="text" class="form-control" name="name" id="name" autocomplete="off"
-                                       required value="<?= $user->name ?>" placeholder="Enter your username here ...">
+                                <input type="text" class="form-control" name="name" id="name" autocomplete="off" required value="<?= $user->name ?>" placeholder="Enter your username here ...">
                             </div>
                         </div>
 
                         <div class="form-group">
                             <div class="offset-md-2 col-sm-10">
                                 <label for="email">E-Mail address</label>
-                                <input type="email" class="form-control" name="email" id="email" autocomplete="off"
-                                       required value="<?= $user->email ?>"
-                                       placeholder="Enter your email address here ...">
+                                <input type="email" class="form-control" name="email" id="email" autocomplete="off" required value="<?= $user->email ?>" placeholder="Enter your email address here ...">
                             </div>
                         </div>
 
-                        <?php if ($isExistingUser): ?>
-                          <div class="form-group">
-                              <div class="offset-md-2 col-sm-10">
-                                  <label for="password">Password</label>
-                                  <div class="input-group">
-                                      <input type="password" class="form-control" name="password" id="password"
-                                             autocomplete="off" required value="<?= $user->getPassword() ?>"
-                                             placeholder="Enter your password here ...">
-                                      <button type="button" class="btn input-group-text"
-                                              onclick="togglePassword(nextSibling)">
+                        <?php if ($isExistingUser) : ?>
+                            <div class="form-group">
+                                <div class="offset-md-2 col-sm-10">
+                                    <label for="password">Password</label>
+                                    <div class="input-group">
+                                        <input type="password" class="form-control" name="password" id="password" autocomplete="off" required placeholder="Enter your new password here ...">
+                                        <button type="button" class="btn input-group-text" onclick="togglePassword(nextSibling)">
                                             <i class="fa fa-fw fa-eye field_icon"></i>
                                         </button>
-                                  </div>
-                              </div>
-                          </div>
+                                    </div>
+                                </div>
+                            </div>
 
-                        <?php else: ?>
-                          <div class="form-group">
-                              <div class="offset-md-2 col-sm-10">
-                                  <label for="password">Password</label>
-                                  <input type="text" class="form-control" name="password" id="password"
-                                         autocomplete="off" required value="<?= $user->password ?>"
-                                         placeholder="Enter your password here ...">
-                              </div>
-                          </div>
-                          <div class="form-group">
-                              <div class="offset-md-2 col-sm-10">
-                                  <label for="password2">Re-type password</label>
-                                  <input type="text" class="form-control" name="password2" id="password2"
-                                         autocomplete="off" required
-                                         placeholder="Re-type your password here ...">
-                              </div>
-                          </div>
+                        <?php else : ?>
+                            <div class="form-group">
+                                <div class="offset-md-2 col-sm-10">
+                                    <label for="password">Password</label>
+                                    <input type="password" class="form-control" name="password" id="password" autocomplete="off" required placeholder="Enter your password here ...">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="offset-md-2 col-sm-10">
+                                    <label for="password2">Re-type password</label>
+                                    <input type="password" class="form-control" name="password2" id="password2" autocomplete="off" required placeholder="Re-type your password here ...">
+                                </div>
+                            </div>
                         <?php endif; ?>
                     </div>
 
@@ -101,36 +89,55 @@
                     }
 
                     function initForm() {
-                        $.validator.setDefaults({ignore: ''})
+                        $.validator.setDefaults({
+                            ignore: ''
+                        })
 
                         $('#userForm').validate({
                             rules: {
-                                name: {required: true, minlength: 4},
-                                email: {required: true, email: true},
-                                password: {required: true, minlength: 6},
-                                password2: {required: true, minlength: 6, equalTo: '#password'},
+                                name: {
+                                    required: true,
+                                    minlength: 4
+                                },
+                                email: {
+                                    required: true,
+                                    email: true
+                                },
+                                password: {
+                                    required: true,
+                                    minlength: 6
+                                },
+                                password2: {
+                                    required: true,
+                                    minlength: 6,
+                                    equalTo: '#password'
+                                },
                             },
                             messages: {
-                                name: {required: "Please enter a username"},
+                                name: {
+                                    required: "Please enter a username"
+                                },
                                 email: {
                                     required: "Please enter an email address",
                                     email: "Please enter a valid email address"
                                 },
-                                password: {required: "Please enter a password"},
+                                password: {
+                                    required: "Please enter a password"
+                                },
                                 password2: {
                                     required: "Please re-type the password",
                                     equalTo: "Passwords must match",
                                 }
                             },
                             errorElement: 'span',
-                            errorPlacement: function (error, element) {
+                            errorPlacement: function(error, element) {
                                 error.addClass('invalid-feedback')
                                 element.closest('div').append(error)
                             },
-                            highlight: function (element, errorClass, validClass) {
+                            highlight: function(element, errorClass, validClass) {
                                 $(element).addClass('is-invalid')
                             },
-                            unhighlight: function (element, errorClass, validClass) {
+                            unhighlight: function(element, errorClass, validClass) {
                                 $(element).removeClass('is-invalid')
                             }
                         })
